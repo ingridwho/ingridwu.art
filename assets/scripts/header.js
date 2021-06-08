cb0e91afb29ddaa0e4d072b532a2fba1b52594fd
notoliver <47568829+notoliver@users.noreply.github.com>
2021-06-08 00:37:19 -0400

    alot of shit

--- assets/scripts/header.js
+++ assets/scripts/header.js
@@ -1,53 +1,30 @@
 //read data in from file
-var level = document.getElementById("header").getAttribute("level");
 var active = document.getElementById("header").getAttribute("active");
 var path =""
-
-//determine relative paths
-if(level==0){
-    //no change
-}
-if(level==1){
-    path="../"
-}
 
 /*
 generate this:
 
 <div class="topnav">
-    <a href="">ingrid wu</a>
-    <a href="">info</a>
-    <a href="">work</a>
-    <a href="play">play</a>
+    <a href="/" class="name">ingrid wu</a>
+    <a href="/">info</a>
+    <a href="/">work</a>
+    <a href="/play">play</a>
 </div>
 */
-var text ="<div class=\'topnav\'>\r\n\t\t\t<a href=\""+path+"\">ingrid wu<\/a>\r\n\t\t\t<a href=\""+path+"\">info<\/a>\r\n\t\t\t<a href=\""+path+"\">work<\/a>\r\n\t\t\t<a href=\'"+path+"play\'>play<\/a>\r\n\t\t<\/div>";
-
-//helper function index of
-function getIndicesOf(searchStr, str, caseSensitive) {
-    var searchStrLen = searchStr.length;
-    if (searchStrLen == 0) {
-        return [];
-    }
-    var startIndex = 0, index, indices = [];
-    if (!caseSensitive) {
-        str = str.toLowerCase();
-        searchStr = searchStr.toLowerCase();
-    }
-    while ((index = str.indexOf(searchStr, startIndex)) > -1) {
-        indices.push(index);
-        startIndex = index + searchStrLen;
-    }
-    return indices;
-}
+var text ="<div class=\'topnav\'>\r\n\t\t\t<a href=\"/\" class=\"name\">ingrid wu<\/a>\r\n\t\t\t<a href=\"\\#about\">info<\/a>\r\n\t\t\t<a href=\"/\">work<\/a>\r\n\t\t\t<a href=\'/play\'>play<\/a>\r\n\t\t<\/div>";
 
 //add active class
-var results = getIndicesOf(active, text,true);
+/*
+WIP
+-needs some better way to account for url/negative length
+*/
+var results = text.indexOf(active);
 if(active=='play'){
-    var firstoccurance=results[0]+5;
+    var firstoccurance=results+5;
 }
 else{
-    var firstoccurance=results[0]+-1;
+    var firstoccurance=results+-1;
 }
 text = text.slice(0, firstoccurance) + " class=\"active\" " + text.slice(firstoccurance);
 
---
+++ assets/scripts/loadmodules.js
@@ -0,0 +1,61 @@
+//init and set data
+var data = window[document.getElementsByClassName("modules")[0].getAttribute("id")];
+var text='';
+var temptext='';
+
+//add to string using a temp string based on module name
+data.forEach(element => {
+    if(element.module=='title'){
+        handleTitle(element.text);
+    }
+    if(element.module=='description'){
+        handleDescription(element.text);
+    }
+    if(element.module=='widephoto'){
+        handleWidePhoto(element.filename);
+    }
+    if(element.module=='back'){
+        handleBack();
+    }
+    if(element.module=='photogrid'){
+        handlePhotogrid(element.filenames);
+    }
+    if (typeof element.inlinestyles !== 'undefined') {
+        handleInlineStyles(element.inlinestyles);
+    }
+    text+=temptext;
+    temptext=''
+});
+
+//helper functions
+function handleTitle(input){
+    temptext+='<div class=\'title\'>'+input+'</div>'
+}
+function handleDescription(input){
+    temptext+='<div class=\'description\'>'+input+'</div>'
+}
+function handleWidePhoto(filename){
+    temptext+='<div class=\'image\'><img src=\''+filename+'\'></img></div>'
+}
+function handlePhotogrid(filenames){
+    temptext+='<div class=\'photogrid\'>';
+
+    
+    filenames.forEach(filename => {
+        //math for width??
+        var ratio = filename.x / filename.y;
+        temptext+='<div style=\'flex: '+ratio+'\'><img src=\''+filename.image+'\'></img></div>'
+    });
+
+    temptext+='</div>';
+}
+function handleBack(){
+    temptext+='<div class=\'back\'><a href=\'/\'>back<\/a></div>'
+}
+function handleInlineStyles(inlinestyles){
+    var firstoccurance = temptext.indexOf('>');
+    temptext = temptext.slice(0, firstoccurance) + " style=\'"+inlinestyles+"\' " + temptext.slice(firstoccurance);
+}
+
+//output to container
+document.getElementsByClassName('modules')[0].innerHTML = text;
